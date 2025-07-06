ef3c17541432ef85829612f2a2c9555fd3c1cd8f
Ifeanyichukwu Sampson <ifeanyidotdev@Ifeanyichukwus-MacBook-Air.local>
2025-07-07 00:33:00 +0100

    updated how selction of type of project to scafold

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import { Command } from "commander";
+import inquirer from "inquirer"
 import { scafoldProject } from "./scafolder";
 
 const program = new Command();
@@ -10,14 +11,29 @@ program
 program
   .command("create <projectName>")
   .description("generates a new project")
-  .option(
-    "-t, --template <template>",
-    "template names (bare-express, bare-hono)",
-    "bare-hono",
-  )
-  .action(async (projectName, options) => {
+  // .option(
+  //   "-t, --template <template>",
+  //   "template names (bare-express, bare-hono)",
+  //   "bare-hono",
+  // )
+  .action(async (projectName, _) => {
+    const opts = await inquirer.prompt([
+      {
+        type: "list",
+        name: "template",
+        message: "Which template are you rooting for",
+        default: "hono",
+        choices: [
+          "express",
+          "express-mongo",
+          "hono",
+          "hono-bun",
+          "hono-bun-mongo"
+        ]
+      }
+    ])
     console.log("Generating a new project...");
-    await scafoldProject(projectName, options.template);
+    await scafoldProject(projectName, opts.template);
   });
 
 program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 #!/usr/bin/env bun
 
 import "./cli";
+
+
+
